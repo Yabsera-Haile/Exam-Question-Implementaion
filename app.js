02ba0dfd8b1513366f10af4536161d5b20130e68
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2022-07-04 17:03:12 +0300

    Initial Upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const message=require('./message');
+
+message.error('This is an Error');
+message.success('This is a Success');
---
+++ message.js
@@ -0,0 +1,11 @@
+const error=function(message)
+{
+    console.log(message);
+}
+const success=function(message)
+{
+    console.log(message);
+}
+
+exports.error=error;
+exports.success=success;
